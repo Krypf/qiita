535743f6660b0490ec80cf42cdaf1d5dbdf3a87d
Krypf <73819781+Krypf@users.noreply.github.com>
2021-04-10 04:34:43 +0900

    Add files via upload

---
+++ testmod.py
@@ -0,0 +1,11 @@
+x = 0
+print(x)
+
+# 1
+def foo(): print('bar')
+# 2
+#def foo(): print('bar1')
+
+if __name__ == '__main__':
+    print('This is a test module named testmod.')
+    print('x = '+str(x))
